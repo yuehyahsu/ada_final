e6f73241351d62bde89b02651252d3b7ff154e3a
Sam Hsu <y.hsu@wustl.edu>
2020-04-14 21:52:19 -0500

    Added approx_age and event variables
    Approx_age is a more precise calculation of the age of the individuals; event is required for surv objects

--- 01_data/04_clean_data.R
+++ 01_data/04_clean_data.R
@@ -1,8 +1,11 @@
 data <- patient_info %>% 
     select(patient_id, sex, birth_year, age, province, city, infection_case, confirmed_date, released_date,
            deceased_date, state) %>% 
-    mutate(resolved_date = coalesce(released_date, deceased_date),
-           survival_days = deceased_date - confirmed_date) %>% 
+    mutate(approx_age = 2020 - birth_year,
+           resolved_date = coalesce(released_date, deceased_date),
+           survival_days = deceased_date - confirmed_date,
+           event = if_else(state == "deceased", 1, 0)) %>% 
+    rename(age_cat = age) %>% 
     left_join(region, by = c("province", "city")) %>% 
     select(-code:-university_count, -nursing_home_count) %>% 
     left_join(weather, by = c("province" = "province", "confirmed_date" = "date")) %>% 
