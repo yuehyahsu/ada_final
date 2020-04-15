cd9d7cc5a2eaaf48b2a558142c15ed519ce1f1ad
Sam Hsu <y.hsu@wustl.edu>
2020-04-14 21:07:05 -0500

    Initial commit

---
+++ 01_data/03_load_data.R
@@ -0,0 +1,8 @@
+# read data -------------------------------------------------------------
+
+case <- read_csv("./01_data/raw/Case.csv")
+patient_info <- read_csv("./01_data/raw/PatientInfo.csv")
+patient_route <- read_csv("./01_data/raw/PatientRoute.csv")
+region <- read_csv("./01_data/raw/Region.csv")
+seoul <- read_csv("./01_data/raw/SeoulFloating.csv")
+weather <- read_csv("./01_data/raw/Weather.csv")
---
+++ README.md
@@ -0,0 +1,2 @@
+# ada_final
+ Final project ADA Spring 2020: survival analysis of Korea COVID-19 data
