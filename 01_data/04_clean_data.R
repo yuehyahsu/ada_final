2354261d2f9e0f0530ef773907d45840a2ae4859
Sam Hsu <y.hsu@wustl.edu>
2020-04-14 21:42:59 -0500

    Corrected survival_days variable
    Survival_days should have been deceased_date - confirmed date

--- 01_data/04_clean_data.R
+++ 01_data/04_clean_data.R
@@ -2,11 +2,11 @@ data <- patient_info %>%
     select(patient_id, sex, birth_year, age, province, city, infection_case, confirmed_date, released_date,
            deceased_date, state) %>% 
     mutate(resolved_date = coalesce(released_date, deceased_date),
-           survival_days = resolved_date - confirmed_date) %>% 
+           survival_days = deceased_date - confirmed_date) %>% 
     left_join(region, by = c("province", "city")) %>% 
     select(-code:-university_count, -nursing_home_count) %>% 
     left_join(weather, by = c("province" = "province", "confirmed_date" = "date")) %>% 
     select(-code, -min_temp, -max_temp, -most_wind_direction) %>% 
     filter(is.na(survival_days) | !(survival_days < 0)) # there are some negative survival days -- bad data?
 
-#saveRDS(data, file = "./01_data/processed/data.rds")
+saveRDS(data, file = "./01_data/processed/data.rds")
